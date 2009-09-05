17dc9670b995e93987343e69a0b3111b4b4abafe
Vladimír Vondruš <mosra@centrum.cz>
2009-09-05 20:12:30 +0000

    Relační tabulky se zobrazením učitelů a předmětů (nejspíše to je odsouzené k zániku, musí se udělat lepší model, oddělit do vlastních tříd, typová kontrola atd.).

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -9,6 +9,14 @@
 #include <QLabel>
 #include <QStyle>
 #include <QStatusBar>
+#include <QSqlDatabase>
+#include <QSqlRelationalTableModel>
+#include <QTableView>
+
+// Workaround pro Qt 4.5 (QSqlRelationalDelegate není bez QT_GUI_LIB definován)
+#define QT_GUI_LIB
+#include <QSqlRelationalDelegate>
+
 #include "configure.h"
 
 namespace Absencoid {
@@ -17,10 +25,17 @@ namespace Absencoid {
 MainWindow::MainWindow(): tabWidget(new QTabWidget(this)) {
     setWindowTitle(tr("Absencoid"));
 
+    /* Databáze */
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("absencoid.db");
+    db.open();
+
+    /* ##################### Taby ########################################### */
+
     tabWidget->setTabPosition(QTabWidget::West);
     tabWidget->setUsesScrollButtons(false);
 
-    /* Tab "Souhrn" */
+    /* -------------------- Tab "Souhrn" ------------------------------------ */
     QWidget* summary = new QWidget(tabWidget);
 
     /* Vrchní řada tlačítek */
@@ -87,10 +102,74 @@ MainWindow::MainWindow(): tabWidget(new QTabWidget(this)) {
     summary->setLayout(summaryLayout);
     tabWidget->addTab(summary, tr("Souhrn"));
 
-    /* Další "dummy" taby */
+    /* --------------------- Učitelé ---------------------------------------- */
+
+    /* Tabulka */
+    QSqlRelationalTableModel* teachersModel = new QSqlRelationalTableModel(this);
+    teachersModel->setTable("teachers");
+    teachersModel->setRelation(0, QSqlRelation("grades", "id", "name"));
+    teachersModel->setHeaderData(0, Qt::Horizontal, tr("Třída"));
+    teachersModel->setHeaderData(1, Qt::Horizontal, tr("ID"));
+    teachersModel->setHeaderData(2, Qt::Horizontal, tr("Jméno"));
+    teachersModel->setHeaderData(3, Qt::Horizontal, tr("Vlastnosti"));
+    teachersModel->select();
+
+    QTableView* teachersView = new QTableView(this);
+    teachersView->setModel(teachersModel);
+
+    /* Tlačítka pro přidání / odebrání učitele */
+    QPushButton* addTeacher = new QPushButton(tr("Přidat učitele"));
+    QPushButton* deleteTeacher = new QPushButton(tr("Odebrat učitele"));
+
+    /* Layout */
+    QVBoxLayout* teachersButtonLayout = new QVBoxLayout;
+    teachersButtonLayout->addWidget(addTeacher, 0, Qt::AlignTop);
+    teachersButtonLayout->addWidget(deleteTeacher, 1, Qt::AlignTop);
+    QHBoxLayout* teachersLayout = new QHBoxLayout;
+    teachersLayout->addWidget(teachersView);
+    teachersLayout->addLayout(teachersButtonLayout);
+
+    QWidget* teachers = new QWidget(this);
+    teachers->setLayout(teachersLayout);
+
+    tabWidget->addTab(teachers, tr("Učitelé"));
+
+    /* --------------------- Předměty --------------------------------------- */
+    /* Tabulka */
+    QSqlRelationalTableModel* classesModel = new QSqlRelationalTableModel(this);
+    classesModel->setTable("classes");
+    classesModel->setRelation(0, QSqlRelation("grades", "id", "name"));
+    classesModel->setRelation(3, QSqlRelation("teachers", "id", "name"));
+    classesModel->setHeaderData(0, Qt::Horizontal, tr("Třída"));
+    classesModel->setHeaderData(1, Qt::Horizontal, tr("ID"));
+    classesModel->setHeaderData(2, Qt::Horizontal, tr("Předmět"));
+    classesModel->setHeaderData(3, Qt::Horizontal, tr("Učitel"));
+    classesModel->setHeaderData(4, Qt::Horizontal, tr("Vlastnosti"));
+    classesModel->select();
+
+    QTableView* classesView = new QTableView(this);
+    classesView->setModel(classesModel);
+    classesView->setItemDelegateForColumn(3, new QSqlRelationalDelegate(classesView));
+
+    /* Tlačítka pro přidání / odebrání třídy */
+    QPushButton* addClass = new QPushButton(tr("Přidat předmět"));
+    QPushButton* deleteClass = new QPushButton(tr("Odebrat předmět"));
+
+    /* Layout */
+    QVBoxLayout* classesButtonLayout = new QVBoxLayout;
+    classesButtonLayout->addWidget(addClass, 0, Qt::AlignTop);
+    classesButtonLayout->addWidget(deleteClass, 1, Qt::AlignTop);
+    QHBoxLayout* classesLayout = new QHBoxLayout;
+    classesLayout->addWidget(classesView);
+    classesLayout->addLayout(classesButtonLayout);
+
+    QWidget* classes = new QWidget(this);
+    classes->setLayout(classesLayout);
+
+    tabWidget->addTab(classes, tr("Předměty"));
+
+    /* --------------------- Další "dummy" taby ----------------------------- */
     tabWidget->addTab(new QLabel(tr("Zde bude seznam absencí")), tr("Absence"));
-    tabWidget->addTab(new QLabel(tr("Zde bude seznam učitelů")), tr("Učitelé"));
-    tabWidget->addTab(new QLabel(tr("Zde bude seznam předmětů")), tr("Předměty"));
     tabWidget->addTab(new QLabel(tr("Zde bude rozvrh")), tr("Rozvrh"));
     tabWidget->addTab(new QLabel(tr("Zde bude seznam přesunutých a změněných hodin")), tr("Suply"));
     tabWidget->addTab(new QLabel(tr("Zde bude seznam odpadnutých hodin a volných dnů")), tr("Volna"));
