3f291b8a09c9df32a21366b4193fedad10137c8a
Vladimír Vondruš <mosra@centrum.cz>
2009-09-30 18:13:46 +0000

    Revize verzování - program má nyní číslo verze příhodné pro normální uživatele (např. 1.0 beta), podrobné číslo verze i se specifikací správcovské nebo uživatelské edice a stávající SVN revizi. Číslo verze se ponechá staré do té doby, než je nová verze připravena, pak se číslo zvýší a udělá se tag.

--- src/configure.h.cmake
+++ src/configure.h.cmake
@@ -1,8 +1,16 @@
 #ifndef CONFIGURE_H
 #define CONFIGURE_H
 
-#define SVN_VERSION "SVN ${SVN_VERSION}"
+#define SVN_VERSION "${SVN_VERSION}"
 
 #cmakedefine ADMIN_VERSION
+
+#ifdef ADMIN_VERSION
+#define APP_VERSION         "0.8a"
+#else
+#define APP_VERSION         "0.8u"
+#endif
+
+#define APP_VERSION_LONG    "1.0-dev"
 
 #endif
